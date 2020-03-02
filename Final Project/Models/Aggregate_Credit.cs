9b252ef8c91c7f46c754c062240a21a164379eb1
sanaz <sanazmarand@gmail.com>
2020-03-02 09:00:06 -0600

    adds to finalproject

---
+++ Final Project/Models/Aggregate_Credit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final_Project.Models
+{
+    public class Aggregate_Credit
+    {
+        public int Credit_ID { get; set; }
+        public int Event_ID { get; set; }
+        public int Result_ID { get; set; }
+        public int Tournament_ID { get; set; }
+        public int Winnings { get; set; }
+    }
+}
